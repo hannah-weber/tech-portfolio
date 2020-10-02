7daa0b9ebb284beadb17dc68a9f0e8ffab1f2797
hannah-weber <57051054+hannah-weber@users.noreply.github.com>
2020-10-02 15:41:11 -0400

    Update siteConfig.js
    Fixed linkedin url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,7 +67,7 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: 'hannah-weber',
+        linkedin: 'hannah-weber-367196197',
         twitter: '',
         github: 'hannah-weber',
     },
