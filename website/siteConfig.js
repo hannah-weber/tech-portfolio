04f1fe270d6eec986688250393a2d99787d789bb
hannah-weber <57051054+hannah-weber@users.noreply.github.com>
2021-01-18 14:29:45 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -29,7 +29,7 @@ const siteConfig = {
 
     // For no header links in the top nav bar -> headerLinks: [],
     headerLinks: [
-        { doc: "about", label: "About" , font: 'Times New Roman'},
+        { doc: "about", label: "About"},
         { doc: "projects", label: "Projects" },
     ],
 
